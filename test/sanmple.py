b99b70e4ca6fdff804aaa2dbf56901d5fb17dbb0
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-01-04 20:52:44 +0900

    トリプルクオーテーション

--- test/sanmple.py
+++ test/sanmple.py
@@ -1,8 +1,6 @@
 #ecoding: UTF-8
 #↑おまじない
-
-print "Hello!\nByeBye"
-print "Tomato is \"very\"delicious"
-print "The browser displays an \
-error message"
+print """Hello!,Endou
+How are you doing?
+See you"""
 
