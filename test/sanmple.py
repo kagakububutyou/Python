f2b05f668eef80456f0c26113c96f84c4eed7aa9
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-01-05 00:27:33 +0900

    文字列の連結

--- test/sanmple.py
+++ test/sanmple.py
@@ -1,5 +1,7 @@
 #ecoding: UTF-8
 #↑おまじない
 
-print "C:\My Document\node\track\test.txt"
-print r"C:\My Document\node\track\test.txt"
+print "Python" + "Web"
+print u"テレビ" + u"画面"
+print u"西暦" + u"2000" + u"年"
+
