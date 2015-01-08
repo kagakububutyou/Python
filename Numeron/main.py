951a6dc3f4f69cfb8c535cf9cfed68c19d45147e
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-01-08 18:56:15 +0900

    文字か数列化の判定

--- Numeron/main.py
+++ Numeron/main.py
@@ -1,4 +1,23 @@
 #coding: UTF-8
 
-print u"test"
+#開始宣言
+def Numeron():
+	hoge = u"Numer0nを開始します"
+	print hoge
+#入力
+def Input():
+	print u"数字を3桁入力してください"
+	a = raw_input()
+	return a
 
+#数字が入力されたのかの確認
+def Number(num):
+		print u"3"
+		print num.isdigit()
+		return num.isdigit()
+
+Numeron()
+print u"1"
+while Number(Input()) == False:
+		Input()
+print u"2"
