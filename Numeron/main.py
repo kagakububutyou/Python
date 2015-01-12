020c60c0aab8b1e14300294db91a2f01af41adab
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-01-12 12:58:47 +0900

    3桁入力

--- Numeron/main.py
+++ Numeron/main.py
@@ -1,23 +1,31 @@
 #coding: UTF-8
 
+PlayerNumber = 56513 
+
 #開始宣言
 def Numeron():
 	hoge = u"Numer0nを開始します"
 	print hoge
 #入力
 def Input():
-	print u"数字を3桁入力してください"
-	a = raw_input()
-	return a
+	print u"違う数字を3桁入力してください"
+	PlayerNumber = raw_input()
+	return PlayerNumber 
 
 #数字が入力されたのかの確認
 def Number(num):
-		print u"3"
+		print num 
+		if len(num) != 3:
+				return False 
+		if num[0] == num[1] or num[0] == num[2] or num[1] == num[2]:
+				return False
 		print num.isdigit()
 		return num.isdigit()
 
 Numeron()
-print u"1"
-while Number(Input()) == False:
-		Input()
-print u"2"
+print u"違う数字を3桁入力してください"
+PlayerNumber = raw_input()
+while Number(PlayerNumber) == False:
+	print u"違う数字を3桁入力してください"
+	PlayerNumber = raw_input()
+print PlayerNumber 
